567048b8da6294dcbafd4c3ad8ffddb1d1c6b499
Ya-Chen Chuang <Ya-Chen.Chuang@bio-techne.com>
2023-05-04 16:54:04 -0700

    Gabor practice edit

--- GaborFeatureBanks.py
+++ GaborFeatureBanks.py
@@ -15,3 +15,37 @@ img = cv2.imread("C:/Users/ya-chen.chuang/Documents/QuPath/SampleImages/HandEcom
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 plt.imshow(img)
 
+img2 = img.reshape(-1)
+
+df = pd.DataFrame()
+df['original image'] = img2
+
+'''
+GaborFilter = cv2.getGaborKernel((50, 50), 1, 1 / 4. * np.pi, np.pi, 0.95, 0, ktype=cv2.CV_32F)
+fimg = cv2.filter2D(img2, cv2.CV_8UC3, GaborFilter)
+filter_img = fimg.reshape(img.shape[0],img.shape[1])
+plt.imshow(filter_img)
+'''
+
+# Generate Gabor features
+num = 1
+
+for sigma in (1,3):
+    for theta in range(4):
+        theta = theta/ 4. * np.pi
+        for lamda in np.arange(0, np.pi, np.pi / 4): 
+            for gamma in (0.05, 0.5, 0.95):
+                Gabor_label = "Gabor" + str(num)
+                
+                GaborFilter = cv2.getGaborKernel((50, 50), sigma, theta, lamda, gamma, 0, ktype=cv2.CV_32F)
+                
+                
+                fimg = cv2.filter2D(img2, cv2.CV_8UC3, GaborFilter)
+                df[Gabor_label] = fimg
+                print(Gabor_label, ': theta=', theta, ': sigma=', sigma, ': lamda=', lamda, ': gamma=', gamma)
+                
+                num += 1
+                
+
+
+df.to_csv("GaborPractice.csv")
